88c2dd2d4f718a0257514d37c127a52b3a7f3f3e
odariusgeorge <43795225+odariusgeorge@users.noreply.github.com>
2018-10-18 10:45:08 +0300

    Add files via upload

---
+++ Laborator3/Figura.java
@@ -0,0 +1,10 @@
+
+public abstract class Figura {
+	public abstract double arie();
+	public abstract double perimetru();
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
